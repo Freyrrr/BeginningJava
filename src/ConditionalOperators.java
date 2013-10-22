3db63de9191de03bd7adaae1dfc2cd99b019cb94
Freyrrr <jon.freyrhotmail.com>
2013-10-22 17:51:03 +0000

    Add Conditial Operators excersise

---
+++ src/ConditionalOperators.java
@@ -0,0 +1,7 @@
+public class ConditionalOperators {
+	public static void main (String[] args) {
+		int age = 45;
+		
+		System.out.println(age > 60 ? "You're older than 60" : "You're younger than 60");
+	}
+}
