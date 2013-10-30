9cc7308f4cc754f8f542787aeea00c7251d2de6a
Freyrrr <jon.freyr@hotmail.com>
2013-10-30 00:05:37 +0000

    Add for loop

---
+++ src/forLoop.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+
+/**
+ * @author Freyr
+ *
+ */
+public class forLoop {
+	public static void main(String args[]) {
+		for (int counter=1;counter<=29;counter++){
+			System.out.println("Counter is at: " +counter);
+		}
+	}
+}
